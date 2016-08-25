fab67d9e466c22e6131b766face5c5ff7690bd9a
GreyZeng <410486047@qq.com>
2016-08-25 16:54:38 +0800

    If applied, this commit will remove unused package.

--- src/main/java/com/hui/advalidationdemo/ADAuthJava.java
+++ src/main/java/com/hui/advalidationdemo/ADAuthJava.java
@@ -1,6 +1,7 @@
 package com.hui.advalidationdemo;
 
-import static com.hui.advalidationdemo.constant.ApplicationConstants.*;
+import static com.hui.advalidationdemo.constant.ApplicationConstants.buildADPath;
+import static com.hui.advalidationdemo.constant.ApplicationConstants.getConfig;
 import static javax.naming.Context.INITIAL_CONTEXT_FACTORY;
 import static javax.naming.Context.PROVIDER_URL;
 import static javax.naming.Context.SECURITY_AUTHENTICATION;
